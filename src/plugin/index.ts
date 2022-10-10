4a9511ef3293ba73aa1d3eabb4c2ff5954aba059
uuuu66 <uuuu66@naver.com>
2022-10-10 17:38:08 +0900

    fix. 초기 설정 변경

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -4,14 +4,9 @@ import { Types } from "../shared/msgTypes";
 figma.showUI(__html__);
 
 figma.ui.onmessage = (msg: MessageType<any>) => {
-  console.log(msg);
   if (msg.type === Types.EX) {
-    console.log(figma.currentPage.findChildren());
-
-    const nodes: SceneNode[] = [];
+    console.log(figma.currentPage.findAll());
     const square = figma.createRectangle();
     figma.currentPage.appendChild(square);
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
   }
 };
